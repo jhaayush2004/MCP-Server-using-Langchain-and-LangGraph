dae6b93281604703eec83994dd80fd4def8fd4ef
Ayush Shaurya Jha <164651751+jhaayush2004@users.noreply.github.com>
2025-06-26 22:51:17 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from langgraph.prebuilt import create_react_agent
 from langchain_groq import ChatGroq
-
+#
 from dotenv import load_dotenv
 load_dotenv()
 
